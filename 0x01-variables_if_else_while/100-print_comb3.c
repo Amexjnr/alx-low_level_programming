4f1fd08532a967734b8c5b4cc98730d5dbffde97
Amexjnr <abdul08120893644@gmail.com>
2023-03-18 18:25:09 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 	int i = '0';
 	int j = '0';
 
-	while (i <= '9')
+	while (i <= '8')
 	{
 		while (j <= '9')
 		{
@@ -21,12 +21,8 @@ int main(void)
 				putchar(j);
 				if (i == '8' && j == '9')
 				{
-					putchar('\n');
-				}
-				else
-				{
 					putchar(',');
-					putchar(' ');
+                                        putchar(' ');
 				}
 			}
 			j++;
@@ -34,6 +30,7 @@ int main(void)
 		j = '0';
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
 
