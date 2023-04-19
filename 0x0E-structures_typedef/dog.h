974a3a8faf7b9f14cddb49786c81e27b24a9f1d8
Amexjnr <abdul08120893644@gmail.com>
2023-04-18 22:06:17 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - A new type of data with describing a dog.
  * @name: name of dog
@@ -18,7 +21,6 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
